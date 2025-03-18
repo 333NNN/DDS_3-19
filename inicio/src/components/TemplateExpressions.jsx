2ed915ef8e74f6b087dff10327d091f8a81942b6
333NNN <nata.cunha@aluno.senai.br>
2025-03-17 21:42:42 -0300

    Adição

--- inicio/src/components/TemplateExpressions.jsx
+++ inicio/src/components/TemplateExpressions.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const TemplateExpressions = () => {
   
-
   let aluno = {
     nome: "",
     turma: "",
     curso: "",
   };
+
   aluno.nome = prompt("Insira seu nome: ");
-  aluno.curso = prompt("Insira seu curso: ")
+  aluno.curso = prompt("Insira seu curso: ");
   aluno.turma = prompt("Insira sua turma: ");
 
   return (
@@ -17,7 +17,6 @@ const TemplateExpressions = () => {
       <h1>Olá, {aluno.nome}</h1>
       <p>Seu curso é: {aluno.curso}</p>
       <p>Sua turma é: {aluno.turma}</p>
-      
     </div>
   );
 };
